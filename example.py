403251d147e3f96f64a0fdaea8a1f09695a67b3c
Dmitrii Rumyancev <rychanya@gmail.com>
2020-02-21 10:14:47 +0300

    add translate files

--- example.py
+++ example.py
@@ -1,34 +1,72 @@
 import requests
-#  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
 
 API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
-URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-def translate_it(text, to_lang):
-    """
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-    :param to_lang:
-    :return:
-    """
 
+def translate_it(text, translation_direction):
+    URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
     params = {
         'key': API_KEY,
         'text': text,
-        'lang': 'ru-{}'.format(to_lang),
+        'lang': translation_direction
     }
+    response = requests.post(URL, params=params)
+    try:
+        response.raise_for_status()
+        texts = response.json().get('text')
+        return ''.join(texts)
+    except requests.exceptions.HTTPError as error:
+        print(error)
 
-    response = requests.get(URL, params=params)
-    json_ = response.json()
-    return ''.join(json_['text'])
+
+def get_langs(lang):
+    URL = 'https://translate.yandex.net/api/v1.5/tr.json/getLangs'
+    params = {
+        'key': API_KEY,
+        'ui': lang
+    }
+    response = requests.post(URL, params=params)
+    try:
+        response.raise_for_status()
+        return set(response.json()['langs'].keys())
+    except requests.exceptions.HTTPError as error:
+        print(error)
 
 
-# print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'no'))
+def make_translation_direction(from_lang='en', to_lang='ru'):
+    langs = get_langs(from_lang)
+    if from_lang in langs and to_lang in langs:
+        return f'{from_lang}-{to_lang}'
+    else:
+        print('Invalide language. Try translate from en to ru.')
+        return('en-ru')
+
+
+def translate_file(in_file_path, out_file_path, from_lang='en', to_lang='ru'):
+    translation_direction = make_translation_direction(from_lang=from_lang, to_lang=to_lang)
+    with open(in_file_path) as in_file:
+        with open(out_file_path, 'w') as out_file:
+            for line in in_file:
+                out_file.write(translate_it(line, translation_direction))
+
 
 if __name__ == '__main__':
-    print(translate_it('привет', 'en'))
+    files = [
+        {
+            'file': 'DE.txt',
+            'language': 'de'
+        },
+        {
+            'file': 'ES.txt',
+            'language': 'es'
+        },
+        {
+            'file': 'FR.txt',
+            'language': 'fr'
+        },
+    ]
+    for file in files:
+        translate_file(
+            file['file'], 
+            'translated-' + file['file'], 
+            file['language'])
